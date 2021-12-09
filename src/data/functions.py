e9bb6917ce5bf1b6dcad0c6b2f23c5e2788acabb
joaquinbogado <joaquinbogado@gmail.com>
2021-12-09 17:13:49 -0300

    Added function to read zeek .log and .log.gz files

---
+++ src/data/functions.py
@@ -0,0 +1,41 @@
+import gzip
+import datetime as dt
+import pandas as pd
+
+class NotAZeekFile(Exception):
+    pass
+
+def read_zeek_header(path):
+    header = dict()
+    try:
+        f = gzip.open(path, 'rt')
+        line = f.readline()
+    except gzip.BadGzipFile:
+        f = open(path, 'rt')
+        line = f.readline()
+    if '#separator' not in line:
+        raise NotAZeekFile
+    header['separator'] = line.rstrip().split()[-1].encode().decode('unicode_escape')
+    line = f.readline()
+    header['set_separator'] = line.rstrip().split()[-1]
+    line = f.readline()
+    header['empty_field'] = line.rstrip().split()[-1]
+    line = f.readline()
+    header['unset_field'] = line.rstrip().split()[-1]
+    line = f.readline()
+    header['path'] = line.rstrip().split()[-1]
+    line = f.readline()
+    header['open'] = dt.datetime.strptime(str(line.rstrip().split()[-1]),"%Y-%m-%d-%H-%M-%S")
+    line = f.readline()
+    header['fields'] = line.rstrip().split()[1:]
+    line = f.readline()
+    header['types'] = line.rstrip().split()[1:]
+    return header
+
+
+
+def read_zeek(path):
+    header = read_zeek_header(path)
+    df = pd.read_csv(path, skiprows=8, names=header['fields'], sep=header['separator'], comment='#')
+    df['ts'] = pd.to_datetime(df.ts, units='s')
+    return df
