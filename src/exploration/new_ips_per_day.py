427226f4c4877f1a705d40f9cf2262801b821e9d
joaquinbogado <joaquinbogado@gmail.com>
2021-12-20 17:07:57 -0300

    Added first explotation scripts (to be used from ipython)

---
+++ src/exploration/new_ips_per_day.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import datetime as dt
+
+df = pd.read_csv(f'../../data/processed/attacks.2021-11-01.csv')
+for date1, date2 in [(x.strftime('%Y-%m-%d'), (x+dt.timedelta(days=1)).strftime('%Y-%m-%d')) for x in pd.date_range(start='2021-11-01', end='2021-12-09')]:
+    df1 = pd.read_csv(f'../../data/processed/attacks.{date1}.csv')
+    df2 = pd.read_csv(f'../../data/processed/attacks.{date2}.csv')
+    oldips = len(df2.merge(df1,on='orig'))
+    print(f'The percentage of new IPs for {date2} with respect to {date1} is {1 - oldips/len(df2):0.2f}')
+    oldips = len(df2.merge(df,on='orig'))
+    print(f'The percentage of new IPs for {date2} with respect to 2021-11-01 is {1 - oldips/len(df2):0.2f}')
+
+ips_hist = dict()
+for date1 in [(x.strftime('%Y-%m-%d')) for x in pd.date_range(start='2021-11-01', end='2021-12-09')]:
+    df = pd.read_csv(f'../../data/processed/attacks.{date1}.csv')
+    for ip in df.itertuples():
+        if ip.orig not in df.keys():
+            ips_hist.setdefault(ip.orig, [])
+        ips_hist[ip.orig].append([dt.date.fromisoformat(date1), ip.count])
+
+stats = []
+df = pd.read_csv(f'../../data/processed/attacks.2021-11-01.csv')
+for date1, date2 in [(x.strftime('%Y-%m-%d'), (x+dt.timedelta(days=1)).strftime('%Y-%m-%d')) for x in pd.date_range(start='2021-11-01', end='2021-12-09')]:
+    df1 = pd.read_csv(f'../../data/processed/attacks.{date1}.csv')
+    df2 = pd.read_csv(f'../../data/processed/attacks.{date2}.csv')
+    oldips1 = len(df2.merge(df1,on='orig'))
+    print(f'The percentage of new IPs for {date2} with respect to {date1} is {1 - oldips1/len(df2):0.2f}')
+    oldips2 = len(df2.merge(df,on='orig'))
+    print(f'The percentage of new IPs for {date2} with respect to 2021-11-01 is {1 - oldips2/len(df2):0.2f}')
+    stats.append([1 - oldips1/len(df2), 1 - oldips2/len(df2)])
+stats = np.array(stats)
+
+plt.plot(pd.date_range(start='2021-11-02', end='2021-12-10'), stats[:,0]*100, label='New IP vs previous day')
+plt.plot(pd.date_range(start='2021-11-02', end='2021-12-10'), stats[:,1]*100, label='New IP vs 2021-11-01')
+plt.title('Comparison of new attackers by day')
+plt.xlabel('Date')
+plt.ylabel('New attackers (in %)')
+plt.legend()
+plt.grid()
